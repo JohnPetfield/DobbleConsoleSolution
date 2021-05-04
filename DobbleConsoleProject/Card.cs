cba6b0bd1ac0aec9a492a64fac860919978f63e4
john petfield <johnmeatjohnmeat@gmail.com>
2021-05-04 14:47:34 +0100

    Add project files.

---
+++ DobbleConsoleProject/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DobbleConsoleProject
+{
+    class Card
+    {
+        /// Hold images in an array or List, these are the indexs for that array
+        public List<int> imageIndexs; // = new List<int>();
+        int x, y;
+
+        public Card(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+            imageIndexs =  new List<int>();
+        }
+
+        public Card(List<int> _imageIndexs)
+        {
+            imageIndexs = _imageIndexs;
+        }
+    }
+}
---
+++ DobbleConsoleProject/Vector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DobbleConsoleProject
+{
+    class Vector
+    {
+        public int x, y;
+
+        public Vector(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+}
